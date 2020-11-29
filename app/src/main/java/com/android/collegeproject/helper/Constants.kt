4aa25a2dcbae0c108d05eb12725544f9966a4b5b
akshat102 <ak.collegework@gmail.com>
2020-11-29 18:37:57 +0530

    adding ip address

--- app/src/main/java/com/android/collegeproject/helper/Constants.kt
+++ app/src/main/java/com/android/collegeproject/helper/Constants.kt
@@ -3,8 +3,7 @@ package com.android.collegeproject.helper
 class Constants {
     val USER_INFO = "USER_INFO"
     val USER_ISFIRSTLOGIN = "USER_ISFIRSTLOGIN"
-
-
+    val IMAGE_HOST = "<YOUR IP ADDRESS>"
 
     fun speak(text: String, mTextToSpeechHelper: TextToSpeechHelper) {
         mTextToSpeechHelper.speakEnglish(text)
